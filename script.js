ffbfddb17d8a371467da71f2002c556ad31021f8
BrandonBishop84 <brandonnovember@gmail.com>
2022-09-07 19:53:48 -0700

    classid

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 console.log('supdog');
-/*----- constants -----
-riddle trivia area / answer box/ 
+/*----- constants -----/
+riddle trivia area / answer box/ riddles
 */
+const buttons = document.getElementById('buttons');
 
-/*----- app's state (variables) -----*/
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const riddle1 = 'What has a head, a tail, is brown, and has no legs?';
+const riddle2 = 'What is more valuable when broken?';
+const riddle3 = 'What gets wet as it dries?';
 
-/*----- cached element references -----*/
+const riddleEl = document.querySelector('#riddle');
+console.log(riddleEl);
+riddleEl.innerText = riddle1;
+buttons.addEventListener('click', function () {
+	button1.innerText = 'hello';
+});
+/*----- cached element references 
+need to have questions appear after completing others
+-----*/
 
 /*----- event listeners -----
 opening prompt/answer box/ scoreboard 
 */
 
 /*----- functions -----
-render function to start new game
+init function
+render function to reset new game
 function for question area to switch to new questions.
 answer area to have function for answer selected.
 function to have score adjusted as progress.
 */
+
+function renderOne() {}
+renderOne();
